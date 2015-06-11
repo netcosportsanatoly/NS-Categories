2da1a4bcb98c0c8edd906c8d7f7a0404c68a3cb6
GuillaumeDerivery <guillaume.derivery@gmail.com>
2015-06-11 19:08:34 +0200

    add methodes in the NSDate_Tool category

--- Extends/NSDate+NSDate_Tool.h
+++ Extends/NSDate+NSDate_Tool.h
@@ -11,7 +11,7 @@
 @interface NSDate (NSDate_Tool)
 
 /**
- *  Create a NSDate instance from a string representing a date-time following the given format.
+ *  Create a NSDate instance (with device timezone & device locale) from a string representing a date-time following the given format.
  *
  *  @param dateTime       Date-time as string
  *  @param dateTimeFormat Format used to describe the date-time
@@ -21,7 +21,7 @@
 +(NSDate *)dateWithString:(NSString *)dateTime followingFormat:(NSString *)dateTimeFormat;
 
 /**
- *  Create a NSDate instance from an ISO 8601 string representation
+ *  Create a NSDate instance (with device timezone & device locale) from an ISO 8601 string representation
  *
  *  @param `dateTime' ISO 8601 string representation
  *
@@ -30,7 +30,7 @@
 +(NSDate *) dateWithISO8601String:(NSString *)dateTimeZFormat;
 
 /**
- *  Transform a NSDate instance to a string representation following the given format
+ *  Transform a NSDate instance (with device timzone & device local) to a string representation following the given format
  *
  *  @param `dateTime' NSDate
  *  @param `dateTimeFormat' Format followed by the output string
@@ -49,10 +49,33 @@
 +(NSString *) stringWithISO8601Date:(NSDate *)dateTime;
 
 /**
+ *  Create a NSDate (with device timezone & device locale) from a gmt timestamp
+ *
+ *  @param `gmtTimeStamp' Timestamp
+ *
+ *  @return New NSDate instance
+ */
++(NSDate *) dateWithTimeStamp:(NSTimeInterval)gmtTimestamp;
+
+/**
  *  Check if self is today or not
  *
  *  @return Boolean value describing if the date is today or not.
  */
 -(BOOL) isToday;
+
+/**
+ *  Create a NSDate instance (with device timezone & device locale) from the current self (NSDate)
+ *
+ *  @return New NSDate instance
+ */
+-(NSDate *) toLocalTime;
+
+/**
+ *  Create a NSDate instance (GMT) from the current self (NSDate)
+ *
+ *  @return New NSDate instance
+ */
+-(NSDate *) toGlobalTime;
 
 @end
