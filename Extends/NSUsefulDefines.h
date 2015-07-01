687eff67057f2698ebc9165a2af415966c81c3be
GuillaumeDerivery <guillaume.derivery@gmail.com>
2015-07-01 17:00:21 +0200

    add new define for delegate implementation checking

--- Extends/NSUsefulDefines.h
+++ Extends/NSUsefulDefines.h
@@ -26,5 +26,6 @@
 
 #pragma mark - Conditions
 #define EXISTS(object, class) (object && [object isKindOfClass:class])
+#define DELEGATE_IMPLEMENTATION(delegate, selector) (delegate && [delegate respondsToSelector:selector])
 
 #endif
